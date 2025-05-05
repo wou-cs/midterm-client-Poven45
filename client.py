3e161db748025370a9bf91709a6cf60a6deb12f1
poven45 <povensangha@ymail.com>
2025-05-04 21:04:55 -0700

    All tests are passing and ready to push, all 3 are made now fully

--- client.py
+++ client.py
@@ -18,7 +18,13 @@ def get_programmer_by_id(pid):
     :param pid: Unique identifier for the programmer to lookup
     :return: A dictionary containing the matched programmer. Return an empty dictionary if not found
     """
-    return {}
+    base_url = "http://chrisbrooks.pythonanywhere.com/"
+    r = requests.get(base_url + f'api/programmers/{pid}')
+    
+    if r.status_code == 200:
+        return r.json()
+    else:
+        return {}
 
 
 def get_full_name_from_first(first_name):
@@ -27,4 +33,12 @@ def get_full_name_from_first(first_name):
     :param first_name:
     :return: A string containing the first and last name of the first programmer in the list of matches.
     """
-    return ""
+    base_url = "http://chrisbrooks.pythonanywhere.com/"
+    r = requests.get(base_url + f'api/programmers/by_first_name/{first_name}')
+    programmers_data = r.json()
+    
+    if 'programmers' in programmers_data and programmers_data['programmers']:
+        programmer = programmers_data['programmers'][0]
+        return f"{programmer['first']} {programmer['last']}"
+    else:
+        return None
